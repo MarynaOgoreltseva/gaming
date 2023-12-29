6211b930744ff444f55136fcd415d918616e9648
MarynaOgoreltseva <ogoreltseva1@gmail.com>
2023-12-29 15:18:14 +0200

    add slider 2

--- js/js.js
+++ js/js.js
@@ -8,3 +8,24 @@ $('#slider-about').slick({
 });
 
 
+$('#slider-services').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 900,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      }
+    },
+    {
+      breakpoint: 475,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      }
+    }
+  ]
+});
